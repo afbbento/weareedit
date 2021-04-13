a53b2ad5ed254720d711775a50e2b10ff27dc99b
André <afbbento@gmail.com>
2021-04-13 02:19:38 +0100

    eventos listagem

---
+++ eventos.php
@@ -0,0 +1,112 @@
+<?php
+/**
+* Template Name: Eventos
+*
+* @package weareedit
+* @subpackage weareedit
+* @since weareedit
+*/
+
+get_header();
+
+?>
+
+<div class="content formacao-area-template">
+            <div class="filter-header">
+                <div class="row">   
+                    <div class="container">
+                        <div class="col-md-12">
+                            <ul class="breadcrumb reveal-block">
+                                <li><a href="#">Eventos</a></li>
+                            </ul>    
+                        </div>      
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="container reveal-block">
+                        <div class="col-md-12">
+                            <div class="filter-select"><span class="select-course-area">Design, </span><span class="unselected select-location">Local</span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <section class="filter-result result-events">
+                <div class="row grid-sp-40 reveal-block">
+                    <div class="container">
+                    <?php
+                    $posts = get_posts(array(
+                        'posts_per_page'	=> 6,
+                        'post_type'			=> 'eventos'
+                    ));
+                    if( $posts ){
+                        $i=1;
+                        foreach( $posts as $post ){
+                            $i++;
+                            setup_postdata( $post );
+                            $link_evento = get_permalink();
+                            $titulo_evento = get_field('titulo'); 
+                            $evento_inicio = get_field('data_inicio');
+                            $imagem = get_field('home_image');
+                            
+                            $evento_inicio_format = DateTime::createFromFormat('Ymd', $evento_inicio); 
+                            $dia_evento = $evento_inicio_format->format('d');
+                            $mes_evento = $evento_inicio_format->format('m');
+
+                            $mes_texto = getMes($mes_evento);
+
+                        if ($i%2==0){
+                            echo '</div></div><div class="row grid-sp-40 reveal-block"><div class="container">';
+                        }
+                        ?>
+                    
+                            <div class="col-sm-6 col-md-6">
+                                <a href="#" class="course-box" style="background-image:url(<?php echo $imagem['url']; ?>"></a>    
+                                <div class="course-text">
+                                    <div class="course-time">Evento</div>
+                                    <div class="course-title"><?php echo $titulo_evento; ?></div>
+                                    <div class="course-date"><?php echo $dia_evento; ?> de <?php echo $mes_texto; ?>, Lisboa</div>
+                                </div>
+                            </div>                                        
+                        
+
+                    <?php
+                    }
+                    
+                wp_reset_postdata();
+                }
+                ?>
+                    </div>
+                </div>                
+            </section>      
+        </div>
+
+        <div class="side-filter-container" >
+            <form id="filter-locations" name="filter-locations">
+                <div class="overlay-side-filter"></div>
+                <div class="side-filter">
+                    
+                    <div class="filter-content">
+                        <div class="close">
+                            <img src="img/fechar.svg" />
+                        </div>            
+                        <div class="filter-label">Local</div>
+                        <div class="filter-options">
+                            <div> 
+                                <input type="radio" value="Lisboa" id="location1" name="location" >
+                                <label for="location1">Lisboa</label>
+                            </div>
+                            <div>
+                                <input type="radio" value="Porto" id="location2" name="location" >
+                                <label for="location2">Porto</label>    
+                            </div>
+                        </div>
+                    </div>
+                    <div class="side-bottom">
+                        <div class="btn-container">
+                            <a href="#" class="btn btn-black get-filter">Guardar Informação</a>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -154,3 +154,22 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 }
 
 include_once dirname(__FILE__) . "/inc/custom-post-types.php";
+
+function getMes($mes){
+    switch ($mes) {
+        case "01":    $mes = Janeiro;     break;
+        case "02":    $mes = Fevereiro;   break;
+        case "03":    $mes = Março;       break;
+        case "04":    $mes = Abril;       break;
+        case "05":    $mes = Maio;        break;
+        case "06":    $mes = Junho;       break;
+        case "07":    $mes = Julho;       break;
+        case "08":    $mes = Agosto;      break;
+        case "09":    $mes = Setembro;    break;
+        case "10":    $mes = Outubro;     break;
+        case "11":    $mes = Novembro;    break;
+        case "12":    $mes = Dezembro;    break; 
+    }
+
+return $mes;
+}
