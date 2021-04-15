8253bfd511353f3e1f8265b24862a20712eb7e4b
André <afbbento@gmail.com>
2021-04-15 23:39:45 +0100

    ajustes recrutamento
    Adicionada classe do body page-template-recrutamento
    Ajustas no bloco text-block-container

--- recrutamento.php
+++ recrutamento.php
@@ -15,25 +15,14 @@ get_header();
                 <ul class="breadcrumb">
                     <li><a href="#">Sobre nós</a></li>
                 </ul>
-                <div class="row">
+                <div class="row reveal-block">
                     <div class="col-md-12">    
-                        <h1 class="reveal-text">Recrutamento</h1>
-                        <p class="reveal-block">A EDIT. tem como constante preocupação a empregabilidade dos seus alunos. </p>
+                        <h1>Recrutamento</h1>
+                        <p>A EDIT. tem como constante preocupação a empregabilidade dos seus alunos. </p>
                     </div>
                 </div>
                 <div class="row grid-sp-40 hero-menu hero-no-image">
-                    <div class="col-md-3">
-                        <a href="recrutamento.php" class="active">Recrutamento</a>
-                    </div>
-                    <div class="col-md-3">
-                        <a href="campus.php">Campus</a>
-                    </div>
-                    <div class="col-md-3">
-                        <a href="alunos.php">Alunos</a>
-                    </div>
-                    <div class="col-md-3">
-                        <a href="den.php">DEN</a>
-                    </div>
+                    <?php get_template_part( 'template-parts/menu', 'escola' ); ?>
                 </div>
             </div>
         </div>
@@ -44,87 +33,88 @@ get_header();
                     <div class="row">
                         <div class="col-md-4"><small class="text-muted ">recrutamento</small></div>
                     </div>
-                    <div class="row">
+                    <div class="row reveal-block">
                         <div class="col-md-4">    
-                            <h2 class="reveal-text section-title">Acelaração da integração no mercado de trabalho</h2>
+                            <h2 class="section-title">Acelaração da integração no mercado de trabalho</h2>
                         </div>
                         <div class="col-md-8">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="text-block-container">
-                                        <div class="col-md-12">
-                                            <h4 class="text-white border">Empregabilidade</h4>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <p>Desde o seu início que a EDIT. tem como constante preocupação a empregabilidade dos seus alunos. Prova disso é a componente prática dos seus Programas, Cursos Intensivos ou Workshops.</p>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <p>Ainda nesta perspetiva, e com o objetivo de dar inputs de valor aos seus alunos, os tutores da EDIT. são profissionais reconhecidos no mercado, com uma experiência relevante e significativa. Após formação os alunos têm ao seu dispor as ferramentas necessárias para uma rápida integração no mercado de trabalho.</p>
-                                        </div>
-                                        <div class="col-md-6 logos-col">
-                                            <p>Plataforma de recrutamento:</p>    
-                                            <div class="logo-container">
-                                                <img src="<?php echo get_template_directory_uri(); ?>/img/edit-work.png">
-                                            </div>       
-                                        </div>
-                                        <div class="col-md-6 logos-col">
-                                            <p>Espaço de cowork:</p>    
-                                            <div class="logo-container">
-                                                <img src="<?php echo get_template_directory_uri(); ?>/img/edit-work.png">
-                                            </div>       
-                                        </div>
+                            <div class="text-block-container">
+                                <div class="row sp-60">
+                                    <div class="col-sm-12">
+                                        <h4 class="text-white border">Empregabilidade</h4>
                                     </div>
-                                    <div class="text-block-container">
-                                        <div class="col-md-12">
-                                            <h4 class="text-white border">Formação</h4>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <p>De acordo com um inquérito de satisfação realizado a ex-alunos da EDIT., a qualidade e o teor dos conteúdos programáticos são os principais motivos pelos quais escolheram a EDIT., bem como o facto de ter tutores profissionais da área.</p>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <p>A grande maioria dos ex-alunos que participaram no inquérito considera que a formação que tiveram na EDIT. teve um impacto positivo na sua carreira profissional (82%), sendo que conseguiram ou uma mudança na carreira, uma progressão, mudar de área ou mesmo mudar de empresa.</p>
-                                        </div>
-                                        <div class="col-md-12 block-btn-container">
-                                            <p>Formação:</p>
-                                            <div class="btn btn-slide">
-                                                <a href="#">Cursos</a>
-                                            </div>
-                                            <div class="btn btn-slide">
-                                                <a href="#">Workshops</a>
-                                            </div>
-                                            <div class="btn btn-slide">
-                                                <a href="#">Cursos intensivos</a>
-                                            </div>
-                                            <div class="btn btn-slide">
-                                                <a href="#">Bootcamps</a>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-md-12 block-btn-container">
-                                            <p>Remote Learning:</p>
-                                            <div class="btn btn-slide">
-                                                <a href="#">Cursos</a>
-                                            </div>
-                                            <div class="btn btn-slide">
-                                                <a href="#">Workshops</a>
-                                            </div>
-                                        </div>              
+                                    <div class="col-sm-6">
+                                        <p>Desde o seu início que a EDIT. tem como constante preocupação a empregabilidade dos seus alunos. Prova disso é a componente prática dos seus Programas, Cursos Intensivos ou Workshops.</p>
                                     </div>
-
-                                    <div class="text-block-container">
-                                        <div class="col-md-12">
-                                            <h4 class="text-white border">Parceiros de recrutamento</h4>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <p>Os nossos parceiros de recrutamento assumem uma postura responsável e positiva face ao recrutamento de novos talentos.
-Enquanto parceiros de recrutamento terão acesso privilegiado ao recrutamento dos nossos alunos ou ex-alunos.</p>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <p>Terão uma participação privilegiada no acompanhamento do projeto 360º e outros projetos desenvolvidos pelos nossos alunos, onde terão a oportunidade de ver os alunos a preparem as suas deliverables e assim avaliarem o seu desempenho e o seu enquadramento nas suas empresas.</p>
-                                        </div>
+                                    <div class="col-sm-6">
+                                        <p>Ainda nesta perspetiva, e com o objetivo de dar inputs de valor aos seus alunos, os tutores da EDIT. são profissionais reconhecidos no mercado, com uma experiência relevante e significativa. Após formação os alunos têm ao seu dispor as ferramentas necessárias para uma rápida integração no mercado de trabalho.</p>
                                     </div>
+                                    <div class="col-md-6 logos-col">
+                                        <p>Plataforma de recrutamento:</p>    
+                                        <div class="logo-container">
+                                            <img src="<?php echo get_template_directory_uri(); ?>/img/edit-work.png">
+                                        </div>       
+                                    </div>
+                                    <div class="col-md-6 logos-col">
+                                        <p>Espaço de cowork:</p>    
+                                        <div class="logo-container">
+                                            <img src="<?php echo get_template_directory_uri(); ?>/img/edit-work.png">
+                                        </div>       
+                                    </div>   
                                 </div>
                             </div>
+
+                            <div class="text-block-container">
+                                <div class="row sp-60">
+                                    <div class="col-md-12">
+                                        <h4 class="text-white border">Formação</h4>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <p>De acordo com um inquérito de satisfação realizado a ex-alunos da EDIT., a qualidade e o teor dos conteúdos programáticos são os principais motivos pelos quais escolheram a EDIT., bem como o facto de ter tutores profissionais da área.</p>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <p>A grande maioria dos ex-alunos que participaram no inquérito considera que a formação que tiveram na EDIT. teve um impacto positivo na sua carreira profissional (82%), sendo que conseguiram ou uma mudança na carreira, uma progressão, mudar de área ou mesmo mudar de empresa.</p>
+                                    </div>
+                                    <div class="col-md-12 block-btn-container">
+                                        <p>Formação:</p>
+                                        <div class="btn btn-slide">
+                                            <a href="#">Cursos</a>
+                                        </div>
+                                        <div class="btn btn-slide">
+                                            <a href="#">Workshops</a>
+                                        </div>
+                                        <div class="btn btn-slide">
+                                            <a href="#">Cursos intensivos</a>
+                                        </div>
+                                        <div class="btn btn-slide">
+                                            <a href="#">Bootcamps</a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 block-btn-container">
+                                        <p>Remote Learning:</p>
+                                        <div class="btn btn-slide">
+                                            <a href="#">Cursos</a>
+                                        </div>
+                                        <div class="btn btn-slide">
+                                            <a href="#">Workshops</a>
+                                        </div>
+                                    </div> 
+                                </div>
+                            </div>
+                            <div class="text-block-container">
+                                <div class="row sp-60">
+                                    <div class="col-md-12">
+                                        <h4 class="text-white border">Parceiros de recrutamento</h4>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <p>Os nossos parceiros de recrutamento assumem uma postura responsável e positiva face ao recrutamento de novos talentos.
+Enquanto parceiros de recrutamento terão acesso privilegiado ao recrutamento dos nossos alunos ou ex-alunos.</p>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <p>Terão uma participação privilegiada no acompanhamento do projeto 360º e outros projetos desenvolvidos pelos nossos alunos, onde terão a oportunidade de ver os alunos a preparem as suas deliverables e assim avaliarem o seu desempenho e o seu enquadramento nas suas empresas.</p>
+                                    </div>
+                                </div>
+                            </div>                    
                         </div>
                     </div>                       
                 </div>
@@ -147,37 +137,37 @@ Enquanto parceiros de recrutamento terão acesso privilegiado ao recrutamento do
                     </div>
                     <div class="col-xs-6 col-md-3 col-sm-6">
                         <div class="grid-box">
-                            <img src="img/burocratik-transp.png">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/burocratik-transp.png">
                         </div>
                     </div>
                    <div class="col-xs-6 col-md-3 col-sm-6">
                         <div class="grid-box">
-                            <img src="img/burocratik-transp.png">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/burocratik-transp.png">
                         </div>
                     </div>
                     <div class="col-xs-6 col-md-3 col-sm-6">
                         <div class="grid-box">
-                            <img src="img/burocratik-transp.png">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/burocratik-transp.png">
                         </div>
                     </div>
                     <div class="col-xs-6 col-md-3 col-sm-6">
                         <div class="grid-box">
-                            <img src="img/burocratik-transp.png">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/burocratik-transp.png">
                         </div>
                     </div>
                     <div class="col-xs-6 col-md-3 col-sm-6">
                         <div class="grid-box">
-                            <img src="img/burocratik-transp.png">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/burocratik-transp.png">
                         </div>
                     </div>
                     <div class="col-xs-6 col-md-3 col-sm-6">
                         <div class="grid-box">
-                            <img src="img/burocratik-transp.png">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/burocratik-transp.png">
                         </div>
                     </div>
                     <div class="col-xs-6 col-md-3 col-sm-6">
                         <div class="grid-box">
-                            <img src="img/burocratik-transp.png">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/burocratik-transp.png">
                         </div>
                     </div>
                 </div>
