f7664116dec16a81eb4849f0d75cbb88d9f0f9d9
André <afbbento@gmail.com>
2021-04-15 22:56:56 +0100

    menu escola urls

--- template-parts/menu-escola.php
+++ template-parts/menu-escola.php
@@ -1,25 +1,25 @@
 <div class="col-md-3">
     <div class="btn btn-slide active">
-        <a href="recrutamento.php">Recrutamento</a>
+        <a href="/escola/recrutamento">Recrutamento</a>
         <span class="icon-edit-arrow-right"></span>
     </div>
 </div>
                     
                     <div class="col-md-3">
                         <div class="btn btn-slide">
-                            <a href="campus.php">Campus</a>
+                            <a href="/escola/campus">Campus</a>
                             <span class="icon-edit-arrow-right"></span>
                         </div>
                     </div>
                     <div class="col-md-3">
                         <div class="btn btn-slide">
-                            <a href="alunos.php">Alunos</a>
+                            <a href="/escola/alunos">Alunos</a>
                             <span class="icon-edit-arrow-right"></span>
                         </div>
                     </div>
                     <div class="col-md-3">
                         <div class="btn btn-slide">
-                            <a href="den.php">DEN</a>
+                            <a href="/escola/den">DEN</a>
                             <span class="icon-edit-arrow-right"></span>
                         </div>
                     </div>
