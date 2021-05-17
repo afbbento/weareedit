9a4dc9dff0719a29f0f2bb7dfd985f191ddfb67d
André <afbbento@gmail.com>
2021-05-18 00:29:46 +0100

    equipa
    organizar codigo
    margin bottom

--- template-parts/equipa.php
+++ template-parts/equipa.php
@@ -1,25 +1,31 @@
-<div class="row persons-grid">
+<?php
+$seccao_equipa = get_field('seccao_equipa');
+if( $seccao_equipa ){
+    $equipa = $seccao_equipa['elemento'];
+    $num_elementos = count($equipa);
+    $aux = array("2", "3", "4", "7", "8", "11", "12");
+    if (in_array($num_elementos, $aux)) {
+        $margin = 'space-bottom';
+    }
+?>
+<div class="row persons-grid <?php echo $margin; ?>">
+<?php
+    if($equipa){
+    $i = 0;
+        foreach($equipa as $elemento){
+        $i++;
+            $link_elemento = get_permalink( $elemento->ID );            
+            $nome = get_field('nome', $elemento->ID);
+            $sobrenome = get_field('sobrenome', $elemento->ID);
+            $cargo = get_field('cargo', $elemento->ID);
+            $empresa = get_field('descricao_elemento', $elemento->ID);
+            $foto = get_field('foto', $elemento->ID);
 
-    <?php
-    $seccao_equipa = get_field('seccao_equipa');
-        if( $seccao_equipa ){
-            $equipa = $seccao_equipa['elemento'];
-            $num_elementos = count($equipa);
-            if($equipa){
-                $i = 0;
-                foreach($equipa as $elemento){
-                $i++;
-                    $link_elemento = get_permalink( $elemento->ID );
-                    
-                    $nome = get_field('nome', $elemento->ID);
-                    $sobrenome = get_field('sobrenome', $elemento->ID);
-                    $cargo = get_field('cargo', $elemento->ID);
-                    $empresa = get_field('descricao_elemento', $elemento->ID);
-                    $foto = get_field('foto', $elemento->ID);
-                    
-                    ?>
+           
 
-    <div class="col-md-3 col-sm-6 col-sm-offset-0 col-xs-12 col-xs-offset-0">
+?>
+
+    <div class="col-md-3 col-sm-6 col-sm-offset-0 col-xs-12 col-xs-offset-0 <?php echo $margin;?>">
         <a href=" <?php echo $link_elemento; ?>">
             <div class="adaptImage reveal-block">
                 <div class="adaptImage__inner">
@@ -42,15 +48,18 @@
         </a>
     </div>
     <?php
-                    if (($i==5 || $i==7 ) && $num_elementos<5){
-                        echo '<div class="col-md-3 col-sm-5"></div>';
-                    }
-                    if (($i==9 ) && $num_elementos>5){
-                        echo '<div class="col-md-3 col-sm-5"></div>';
-                    }
-                    }
-                }
+            if (($i==5 || $i==7 ) && $num_elementos<5){
+                echo '<div class="col-md-3 col-sm-5"></div>';
             }
-?>
+            if (($i==9 ) && $num_elementos>5){
+                echo '<div class="col-md-3 col-sm-5"></div>';
+            }
+        }//end foreach
+    }//end if       
+    ?>
 
 </div>
+<?php
+//end equipa block
+}
+?>
